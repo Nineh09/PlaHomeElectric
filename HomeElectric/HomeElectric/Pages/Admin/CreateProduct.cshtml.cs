92f88f3e33b95d0ef7a01a021c08627004a2772b
haocetu <nhathao230502@gmail.com>
2024-04-23 23:58:48 +0700

    form thô createproduct

--- HomeElectric/HomeElectric/Pages/Admin/CreateProduct.cshtml.cs
+++ HomeElectric/HomeElectric/Pages/Admin/CreateProduct.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BusinessObject;
 
 namespace HomeElectric.Pages.Admin
 {
     public class CreateProductModel : PageModel
     {
-        public void OnGet()
+        private readonly YourDbContext _context;
+
+        public CreateProductModel(YourDbContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Products.Add(Product);
+            _context.SaveChanges();
+
+            return RedirectToPage("/Admin/ProductList");
         }
     }
 }
