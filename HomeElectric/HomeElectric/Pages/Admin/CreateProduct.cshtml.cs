ec47bf5ef79b73d44223af56d3cd8162cd96c146
haocetu <nhathao230502@gmail.com>
2024-04-23 23:56:18 +0700

    base createproduct

---
+++ HomeElectric/HomeElectric/Pages/Admin/CreateProduct.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HomeElectric.Pages.Admin
+{
+    public class CreateProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
