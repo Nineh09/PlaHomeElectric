5903c8cd4e482a6dd0230a2d81cdfb661dd81106
Nineh09 <100771455+Nineh09@users.noreply.github.com>
2024-04-24 19:23:16 +0700

    Admin & Staff

---
+++ HomeElectric/HomeElectric/Pages/Staff/StaffDashBoard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HomeElectric.Pages.Staff
+{
+    public class StaffDashBoardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
