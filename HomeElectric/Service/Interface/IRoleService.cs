b2548553a1bc08e426f53e7b9ec18f07a90e92ef
Nineh09 <100771455+Nineh09@users.noreply.github.com>
2024-04-25 02:37:28 +0700

    FullAdmin

---
+++ HomeElectric/Service/Interface/IRoleService.cs
@@ -0,0 +1,15 @@
+﻿using BusinessObject;
+using Repository;
+using Service.Implement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interface
+{
+    public interface IRoleService : IGenericService<Role>
+    {
+    }
+}
