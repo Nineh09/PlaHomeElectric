402f229fa2b32e10407ad9e54fd927581ee6d0e8
Toan Nang <112840020+Le-Nguyen-Toan-Nang@users.noreply.github.com>
2024-04-25 08:29:59 +0700

    update shop detail

--- HomeElectric/Service/Interface/IFeedbackService.cs
+++ HomeElectric/Service/Interface/IFeedbackService.cs
@@ -1,11 +1,5 @@
 ﻿using BusinessObject;
 using Service.Implement;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Service.Interface
 {
     public interface IFeedbackService : IGenericService<Feedback>
